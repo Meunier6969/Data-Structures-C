6874ef302afd7efc4e8b45a2be2bfb19c9975672
Meunier A <123185224+Meunier6969@users.noreply.github.com>
2023-10-21 00:22:59 +0200

    Update

--- headers/binary_tree.h
+++ headers/binary_tree.h
@@ -1,20 +1,26 @@
-#pragma once
+#pragma once 
 
-typedef struct bt_leaf
+typedef struct bt_node
 {
     int value;
-    struct bt_leaf *left;
-    struct bt_leaf *right;
-} bt_leaf;
+    struct bt_node *left;
+    struct bt_node *right;
+} bt_node;
 
 typedef struct bt_tree
 {
-    bt_leaf *root;
+    bt_node *root;
 } bt_tree;
 
-bt_leaf *createLeaf(int value);
+bt_node* BT_createNode(int value);
 
-void insertInOrder(bt_leaf *rootOfTree, int value);
+void BT_insertInOrder(bt_node *root, int value);
+bt_node* BT_searchNode(bt_node *root, int value);
+void BT_deleteNode(bt_node *node, int value);
 
-int heightOfTree(bt_leaf *tree);
-int isTreeEmpty(bt_tree tree);
+void BT_displayPostfix(bt_node* node);
+void BT_displayPrefix(bt_node* node);
+void BT_displayInfix(bt_node* node);
+
+int BT_heightOfTree(bt_node *root);
+int BT_isTreeEmpty(bt_node *tree);
