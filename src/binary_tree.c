8fe02de14f85d5b8d90148d9cfc11928c33e3a30
Meunier A <123185224+Meunier6969@users.noreply.github.com>
2023-10-21 00:23:31 +0200

    Update

--- src/binary_tree.c
+++ src/binary_tree.c
@@ -1,28 +1,114 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
 #include "../headers/binary_tree.h"
 
-bt_leaf *createLeaf(int value)
+bt_node *BT_createNode(int value)
 {
-    bt_leaf *newLeaf = (bt_leaf*) malloc(sizeof(bt_leaf)); // animal crossing real ??????
+    bt_node *newNode = (bt_node*) malloc(sizeof(bt_node));
     
-    newLeaf->value = value;
-    newLeaf->left = NULL;
-    newLeaf->right = NULL;
+    newNode->value = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
 
-    return newLeaf;
+    return newNode;
 }
 
-int heightOfTree(bt_leaf *rootOfTree)
+void BT_insertInOrder(bt_node *node, int value)
 {
-    if (rootOfTree == NULL)
+    if (node == NULL)
+        return;
+
+    if (value < node->value)
+    {
+        if (node->left == NULL)
+        {
+            bt_node* newNode = BT_createNode(value);
+            node->left = newNode;
+        }
+        else
+        {
+            BT_insertInOrder(node->left, value);
+        }
+    }
+
+    if (value >= node->value)
+    {
+        if (node->right == NULL)
+        {
+            bt_node* newNode = BT_createNode(value);
+            node->right = newNode;
+        }
+        else
+        {
+            BT_insertInOrder(node->right, value);
+        }
+    }
+}
+
+bt_node* BT_searchNode(bt_node *node, int value)
+{
+    if (node == NULL)
+        return NULL;
+
+    if (value < node->value)
+        return BT_searchNode(node->left, value);
+
+    if (value > node->value)
+        return BT_searchNode(node->right, value);
+        
+    if (value == node->value)
+        return node;
+}
+
+void BT_deleteNode(bt_node *node, int value)
+{
+    if (node == NULL)
+        return;
+        
+    // Soon™️
+}
+
+void BT_displayPostfix(bt_node* node)
+{
+    if (node == NULL)
+        return;
+
+    BT_displayPostfix(node->left);
+    BT_displayPostfix(node->right);
+    printf("%d ", node->value);
+}
+
+void BT_displayPrefix(bt_node* node)
+{
+    if (node == NULL)
+        return;
+
+    printf("%d ", node->value);
+    BT_displayPrefix(node->left);
+    BT_displayPrefix(node->right);
+}
+
+void BT_displayInfix(bt_node* node)
+{
+    if (node == NULL)
+        return;
+
+    BT_displayInfix(node->left);
+    printf("%d ", node->value);
+    BT_displayInfix(node->right);
+}
+
+int BT_heightOfTree(bt_node *root)
+{
+    if (root == NULL)
         return 0;
     
-    return 1 + fmax(heightOfTree(rootOfTree->left), heightOfTree(rootOfTree->right));
+    return 1 + fmax(BT_heightOfTree(root->left), BT_heightOfTree(root->right));
 }
 
-int isTreeEmpty(bt_tree tree)
+int BT_isTreeEmpty(bt_node *tree)
 {
-    return (tree.root == NULL);
+    return (tree == NULL);
 }
